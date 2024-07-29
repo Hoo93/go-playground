17db5f93bc85b7cfa0893997e1c94575e7bfed75
Hoo93 <sksk8922@gmail.com>
2024-07-30 00:52:10 +0900

    add: 타입 출력 함수 구현

--- go.go
+++ go.go
@@ -1,14 +1,46 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+    "math"
+)
 
-import "rsc.io/quote"
-	
-	
+func sqrt(x float64) string {
+    if x < 0 {
+	return sqrt(-x) + "i"
+    }
+    return fmt.Sprint(math.Sqrt(x))
+}
+
+func pow(x, n, lim float64) float64 {
+    if v := math.Pow(x, n); v < lim {
+	return v
+    } else {
+	fmt.Printf("%g >= %g\n", v, lim)
+    }
+    return lim
+}
+
+func printType(v interface{}) {
+    fmt.Printf("variable : %v , Type : %T\n",v ,v)
+}
 
 func main() {
-    fmt.Println("Hello, World!")
-    fmt.Println(quote.Go())
+    fmt.Println("When's a Saturday?")
+    today := time.Now().Weekday()
+    printType(today)
+    switch time.Saturday {
+    case today + 0:
+	fmt.Println("Today. ")
+    case today + 1:
+	fmt.Println("Tomorrow.")
+    case today + 2:
+	fmt.Println("In two days.")
+    default:
+	fmt.Println("Too far away.")
+    } 
+
 }
 
 
